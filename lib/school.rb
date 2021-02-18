3d09def68a3e2dc31ebfcfd0d1484a7ea080d1c6
Alexander Gould <alexander.gould94@gmail.com>
2021-02-18 13:46:02 -0800

    commit tree e2937582a81eba149784bf533b73ca0ad1fe27df

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,38 @@
 # code here!
+require 'pry'
+class School
+    attr_reader :name
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def roster
+      @roster
+    end
+
+     def roster=(roster)
+        @roster = roster
+     end
+
+    def add_student(student, grade)
+        if @roster.key?(grade)
+            @roster[grade] << (student)
+        else
+            @roster[grade]= []
+            @roster[grade] << student
+        end
+        #
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+    
+    def sort
+        @roster.each do |key, value|
+            value.sort!
+            end
+        #binding.pry
+    end
+end
